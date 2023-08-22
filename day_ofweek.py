7b2c4daacb0673830e4516a8ab0fa395b00e5196
user7217 <sriharshamallavarapu@gmail.com>
2023-08-22 21:08:38 +0530

    more files

---
+++ day_ofweek.py
@@ -0,0 +1,3 @@
+days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"] #list of days of week in order
+day = int(input("Day (0-7)? ")) #ask user for day of week
+print(days[day-1]) #print day of week
--- sum_allnums.py
+++ sum_allnums.py
@@ -2,6 +2,6 @@ num = int(input("enter a number: "))
 sum = 0
 product = 1
 for i in range(0, num):
-    sum +=
+    sum += i
     product = product*i
 print(sum, product)
