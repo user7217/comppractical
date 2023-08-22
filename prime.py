cea2ddf0a4e4f081e4b7d5bea3f73c5f9df29471
user7217 <sriharshamallavarapu@gmail.com>
2023-08-22 21:15:56 +0530

    changes

--- prime.py
+++ prime.py
@@ -1,12 +1,13 @@
 #finding prime numbers
-num = int(input("enter a number"))
+num = int(input("enter a number: "))
 
 if num ==0 or num==1:
     print(num," is neither prime nor composite")
 else:
-    for i in range(2,num):
+    for i in range(2,int(num/2)):
         if num%i ==0:
             print("the number isnt prime")
             break
-
-print("the number is prime")
+        else:
+            print("the number is prime")
+            break
