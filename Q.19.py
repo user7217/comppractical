d467d8aee4c22283e1ebcd6c5e99a181650f4758
user7217 <sriharshamallavarapu@gmail.com>
2023-08-25 16:51:16 +0530

    comments fucking comments

--- Q.19.py
+++ Q.19.py
@@ -1,12 +1,12 @@
 n = int(input("enter a number: "))
 temp = n
 sum = 0
-
-while n > 0:
-    rem = n%10
-    sum += rem**3
-    n //= 10
-if temp == sum:
-    print(temp, "is an armstrong number")
+ 
+while n > 0: #while n is greater than 0
+    rem = n%10 #remainder of n/10
+    sum += rem**3 #sum is sum + rem^3
+    n //= 10 #n is n/10
+if temp == sum:     #if temp is equal to sum
+    print(temp, "is an armstrong number") 
 else:
     print(temp, "is not an armstrong number")  
