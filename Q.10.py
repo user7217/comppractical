d2a72e2acd9f9070a2a48a1d8c382b150619e3bb
user7217 <sriharshamallavarapu@gmail.com>
2023-08-25 16:33:37 +0530

    small changes

--- Q.10.py
+++ Q.10.py
@@ -1,7 +1,7 @@
 num = int(input("enter a number: "))
 sum = 0
 product = 1
-for i in range(0, num):
+for i in range(1, num+1):
     sum += i
     product = product*i
 print(sum, product)
--- Q.12.py
+++ Q.12.py
@@ -1,6 +1,6 @@
 #factors of a number 
 num = int(input("Enter a number: ")) #input number
 print("Factors of",num,"are: ") #print factors of the number
-for i in range(1,int((num+1)/2)): #loop from 1 to num
+for i in range(1,(num+1)): #loop from 1 to num
     if num%i==0: #if i is a factor of num
         print(i) #print i
