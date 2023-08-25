f258e820259c6f6ac54d907cfb33ca90a9cca931
user7217 <sriharshamallavarapu@gmail.com>
2023-08-25 16:37:51 +0530

    changes

--- Q.13.py
+++ Q.13.py
@@ -1,7 +1,7 @@
 #o print all the numbers in the given range divisible by a given number 
-num = int(input("enter a number: "))
-div = int(input("enter a number to divide by: "))
-for i in range(0, num):
-    if i%div ==0:
-        print(i)
+num = int(input("enter the range : ")) #range of numbers
+div = int(input("enter a number to divide by: ")) #number to divide by
+for i in range(0, num): #loop from 0 to num
+    if i%div ==0: #if i is divisible by div
+        print(i) #print i
         
