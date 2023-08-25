7c49adb4b124971b6e7d818184982390ed1db161
user7217 <sriharshamallavarapu@gmail.com>
2023-08-25 16:54:53 +0530

    MORE BLOODY COMMENTS THE CODE ISNT THAT FUCKING

--- Q.20.py
+++ Q.20.py
@@ -1,10 +1,10 @@
 import math
 
-x = int(input("enter  number x: "))
-n = int(input("enter  number n: "))
-def exp(x,n):
-    series = 1
-    for i in range(1,n+1):
-        series += x**i/math.factorial(i)
-    return series
-print(exp(x,n))
+x = int(input("enter  number x: ")) #x is the number
+n = int(input("enter  number n: ")) #n is the number of terms
+def exp(x,n): #function to calculate the exponential series
+    series = 1 #first term of the series is 1
+    for i in range(1,n+1): #loop from 1 to n+1 (n+1 because range is exclusive)
+        series += x**i/math.factorial(i) #series is the sum of the previous term and the next term
+    return series #return the series
+print(exp(x,n)) #print the series
