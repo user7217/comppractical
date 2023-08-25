a50cc644a4f557fb61db178041928dbac92c3132
user7217 <sriharshamallavarapu@gmail.com>
2023-08-25 16:47:07 +0530

    ghgh

--- Q.17.py
+++ Q.17.py
@@ -2,5 +2,6 @@ def fibonacci(n):
     f = [0, 1] #list of first two fibonacci numbers
     for i in range(2, n+1): #for loop to iterate through the list from 2 to n+1 (n+1 because range is exclusive) 
         f.append(f[i-1] + f[i-2]) #new val of the list is the sum of the previous two values in the list before f[i]
-    return f[n] #return the nth value of the list
-print(fibonacci(9))
+    return f[n],f #return the nth value of the list
+num = int(input("enter a number: "))
+print("The fibonacci series of numbers till", num, "is", fibonacci(num))
