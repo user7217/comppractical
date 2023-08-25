f2b4d306733fa58f7d1eaf750f9ff379999eee11
user7217 <sriharshamallavarapu@gmail.com>
2023-08-25 16:57:14 +0530

    COMMENTS FUCKING COMMENTS

--- Q.21.py
+++ Q.21.py
@@ -1,13 +1,13 @@
-def palindrome(s):
-    rev = ''.join(reversed(s))
-    if (s == rev):
-        return True
-    return False
+def palindrome(s): #function to check string is palindrome or not
+    rev = ''.join(reversed(s))   #reversed() function returns the reversed iterator of the given string
+    if (s == rev): #checking if the string is equal to its reverse
+        return True #return true if it is palindrome
+    return False #return false if it is not palindrome
  
-s = str(input("Enter a string: "))
-ans = palindrome(s)
+s = str(input("Enter a string: ")) #input string
+ans = palindrome(s) #calling the function
  
-if (ans):
-    print("Yes")
-else:
-    print("No")
+if (ans): #if ans is true
+    print("Yes") #print yes
+else: #else
+    print("No") #print no
